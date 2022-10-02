7111528a1c97c61c697b2235ac126182b80b2a69
asciito <ayax.cordova@aydev.mx>
2022-10-02 01:18:19 -0500

    :white_check_mark: Test for the rest of the routes

--- tests/Feature/CreatePostTest.php
+++ tests/Feature/CreatePostTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use asciito\BlogPackage\Tests\TestCase;
 use asciito\BlogPackage\Models\Post;
 use asciito\BlogPackage\Tests\User;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class CreatePostTest extends TestCase
 {
@@ -31,5 +32,68 @@ class CreatePostTest extends TestCase
             $this->assertEquals('My first fake body', $post->body);
             $response->assertRedirect(route('posts.show', $post));
         });
+    }
+
+    /** @test */
+    public function a_post_requires_a_title_and_a_body()
+    {
+        $author = User::factory()->create();
+
+        $this->actingAs($author)->post(route('posts.store'), [
+            'title' => '',
+            'body' => 'Some valid body',
+        ])->assertSessionHasErrors('title');
+
+        $this->actingAs($author)->post(route('posts.store'), [
+            'title' => 'Some valid title',
+            'body' => '',
+        ])->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function guest_can_not_create_a_post()
+    {
+        // We're starting from an authenticated state
+        $this->assertFalse(auth()->check());
+
+        $this->post(route('posts.store'), [
+            'title' => 'Some random title',
+            'body' => 'Some random body',
+        ])->assertForbidden();
+    }
+
+    /** @test */
+    public function all_post_are_shown_via_the_index_route()
+    {
+        $this->withoutExceptionHandling();
+        Post::factory()->create([
+            'title' => 'Post number 1',
+        ]);
+        Post::factory()->create([
+            'title' => 'Post number 2',
+        ]);
+        Post::factory()->create([
+            'title' => 'Post number 3',
+        ]);
+
+        $this->get(route('posts.index'))
+            ->assertSee('Post number 1')
+            ->assertSee('Post number 2')
+            ->assertSee('Post number 3')
+            ->assertDontSee('Post number 4');
+    }
+
+    /** @test */
+    public function a_single_post_is_shown_via_the_show_route()
+    {
+        $this->withoutExceptionHandling();
+        $post = Post::factory()->create([
+            'title' => 'The single post title',
+            'body' => 'The single post body',
+        ]);
+
+        $this->get(route('posts.show', $post))
+            ->assertSee('The single post title')
+            ->assertSee('The single post body');
     }
 }
